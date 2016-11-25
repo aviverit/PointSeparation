df082dfea71b277ddc3c957620c0d4370f82668b
aviverit <andrew.viverito@gmail.com>
2016-11-25 15:51:36 -0600

    Separator Class Functions Started (v.0.0.3)

--- src/Separator.java
+++ src/Separator.java
@@ -3,11 +3,45 @@ import java.util.List;
 
 public class Separator {
 
+    private int lineCount=0;
     private int coordCount=0;
+    private String line;
     private List<int[]> coordList = new ArrayList<int[]>();
+    private List<String> lineList = new ArrayList<String>();
 
     Separator (int cC, List cL){
         coordCount=cC;
         coordList=cL;
     }
+
+    void setLineCount(){
+        this.lineCount++;
+    }
+
+    void setLines(){
+        while(coordList!=null){
+            calculateLine();
+        }
+    }
+
+    void appendLines(){
+        if(true){
+            lineList.add("h"+line);
+        }else{
+            lineList.add("v"+line);
+        }
+    }
+
+    void calculateLine(){
+
+    }
+
+    int getLineCount(){
+        return this.lineCount;
+    }
+
+    List<String> getLines(){
+        return lineList;
+    }
+
 }
