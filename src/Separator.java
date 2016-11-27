73fba6e2443072705c1f151d71d82182dc25006c
aviverit <andrew.viverito@gmail.com>
2016-11-27 13:30:27 -0600

    calculateLine upgraded (v.0.0.7)

--- src/Separator.java
+++ src/Separator.java
@@ -8,6 +8,8 @@ public class Separator {
     private int line;
     private List<int[]> coordList = new ArrayList<int[]>();
     private List<String> lineList = new ArrayList<String>();
+    private int orientation=0; //0 = h, 1=v
+    List<int[]> tempCoordList = coordList;
 
     Separator (int cC, List cL){
         coordCount=cC;
@@ -22,7 +24,8 @@ public class Separator {
     }
 
     public void appendLine(){
-        if(true){
+        lineCount++;
+        if(orientation==1){
             lineList.add("h "+new Integer(line).toString()+" \n");
         }else{
             lineList.add("v "+new Integer(line).toString()+" \n");
@@ -31,7 +34,24 @@ public class Separator {
 
     public void calculateLine(){
         line=0;
-        lineCount++;
+        if(orientation==1){
+            orientation=0;
+        }else{
+            orientation=1;
+        }
+
+        int segmentablePoints=0;
+        int currentDistance=0;
+
+        while(segmentablePoints<(tempCoordList.size()/2)){
+            for(int i=0; i<tempCoordList.size();i++){
+                if(currentDistance==tempCoordList.get(i)[orientation]){
+                    segmentablePoints++;
+                }
+            }
+            currentDistance++;
+        }
+        line=currentDistance;
     }
 
     public int getLineCount(){
