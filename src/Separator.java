fbce9320db3a9ad1366d4afc23dd017574504f59
aviverit <andrew.viverito@gmail.com>
2016-11-27 14:57:17 -0600

    calculateLine upgraded, main for loop never completes one cycle (v.0.0.8)

--- src/Separator.java
+++ src/Separator.java
@@ -6,10 +6,12 @@ public class Separator {
     private int lineCount=0;
     private int coordCount=0;
     private int line;
+    private int orientation=0; //0 = h, 1=v
     private List<int[]> coordList = new ArrayList<int[]>();
     private List<String> lineList = new ArrayList<String>();
-    private int orientation=0; //0 = h, 1=v
     List<int[]> tempCoordList = coordList;
+    private List<List<int[]>> collectionList = new ArrayList<List<int[]>>();
+
 
     Separator (int cC, List cL){
         coordCount=cC;
@@ -26,13 +28,18 @@ public class Separator {
     public void appendLine(){
         lineCount++;
         if(orientation==1){
-            lineList.add("h "+new Integer(line).toString()+" \n");
+            lineList.add("h "+ Integer.toString(line) +" \n");
         }else{
-            lineList.add("v "+new Integer(line).toString()+" \n");
+            lineList.add("v "+ Integer.toString(line) +" \n");
         }
     }
 
     public void calculateLine(){
+        int segmentablePoints=0;
+        int currentDistance=0;
+        List<int[]> subCollection1 = new ArrayList<int[]>();
+        List<int[]> subCollection2 = new ArrayList<int[]>();
+
         line=0;
         if(orientation==1){
             orientation=0;
@@ -40,17 +47,28 @@ public class Separator {
             orientation=1;
         }
 
-        int segmentablePoints=0;
-        int currentDistance=0;
-
         while(segmentablePoints<(tempCoordList.size()/2)){
             for(int i=0; i<tempCoordList.size();i++){
                 if(currentDistance==tempCoordList.get(i)[orientation]){
                     segmentablePoints++;
+                    subCollection1.add(tempCoordList.get(i)); //Will work on all points equidistant from axis.
                 }
             }
             currentDistance++;
         }
+        for(int j=0;j<collectionList.size();j++){
+            collectionList.get(j).removeAll(subCollection1);
+            collectionList.get(j).removeAll(subCollection2);
+            if(collectionList.get(j).size()==1){
+                coordList.remove(collectionList.get(j).get(0));
+            }
+        }
+
+        subCollection2.addAll(tempCoordList);
+        subCollection2.removeAll(subCollection1);
+        collectionList.add(subCollection1);
+        collectionList.add(subCollection2);
+
         line=currentDistance;
     }
 
